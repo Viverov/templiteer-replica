3b1925e74a8b50eadb7c868941690adb5c2aff04
Viverov <viverofffff@gmail.com>
2023-09-26 23:57:51 +0300

    Add cors, versioning

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,12 @@
 import { Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import { ExtendedController } from '@libs/nest/ExtendedController';
 
 @ExtendedController({
     version: '1',
 })
 export class AppController {
-    constructor(private readonly appService: AppService) {}
-
-    @Get()
-    getHello(): string {
-        return this.appService.getHello();
+    @Get('/ping')
+    getPing(): string {
+        return 'pong';
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import { RequestMethod, VersioningType } from '@nestjs/common';
 
 async function bootstrap(): Promise<void> {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, new FastifyAdapter());
+
+    app.enableCors({
+        origin: '*',
+    });
+    app.setGlobalPrefix('api', {
+        exclude: [{ path: '/', method: RequestMethod.GET }],
+    });
+    app.enableVersioning({
+        type: VersioningType.URI,
+    });
+
     await app.listen(3000);
 }
 void bootstrap();
