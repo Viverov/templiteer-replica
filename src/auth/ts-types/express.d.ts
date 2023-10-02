f8b7c163e8d348c4c2a787754052f60949232ef7
Viverov <viverofffff@gmail.com>
2023-10-02 20:28:13 +0300

    Fix express request type and remove passport libs

---
+++ src/auth/ts-types/express.d.ts
@@ -0,0 +1,13 @@
+// src/types/express/index.d.ts
+
+import { UserAuthInfo } from '@src/auth/user-auth-info';
+
+export {};
+
+declare global {
+    namespace Express {
+        export interface Request {
+            user?: UserAuthInfo;
+        }
+    }
+}
