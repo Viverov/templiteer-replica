745a17683c47a598e8c28c0750177942a1899ef9
Viverov <viverofffff@gmail.com>
2023-10-05 05:50:29 +0300

    Lint

--- src/web/software/controller-types/find-software.query.ts
+++ src/web/software/controller-types/find-software.query.ts
@@ -1,5 +1,5 @@
-import {IsNumber, IsOptional, IsString, Max} from "class-validator";
-import {ApiProperty} from "@nestjs/swagger";
+import { IsNumber, IsOptional, IsString, Max } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class FindSoftwareQuery {
     @IsOptional()
